9f1e82bc17d2f67dd84c9fd0e4a806db20c13d2f
SibiAkkash <sibi.akkash@gmail.com>
2024-06-21 13:08:23 +0530

    alembic: Set DB url in env.py file

--- server/alembic/env.py
+++ server/alembic/env.py
@@ -6,14 +6,25 @@ from sqlalchemy import pool
 from alembic import context
 
 import os
+import sys
 from db import models
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+
+DB_URL = os.getenv("DB_URL")
+if not DB_URL:
+    print("DB_URL env variable is not set !! Exiting...")
+    sys.exit(0)
+
+config.set_main_option("sqlalchemy.url", DB_URL)
+config.set_main_option("sqlalchemy.echo", "true")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -44,8 +55,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    # url = config.get_main_option("sqlalchemy.url")
-    url = os.getenv("DB_URL")
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -64,25 +74,14 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    # connectable = engine_from_config(
-    #     config.get_section(config.config_ini_section, {}),
-    #     prefix="sqlalchemy.",
-    #     poolclass=pool.NullPool,
-    # )
-
     connectable = engine_from_config(
-        {
-            "url": os.getenv("DB_URL"),
-            "echo": True
-        },
-        prefix="",
-        poolclass=pool.NullPool
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
